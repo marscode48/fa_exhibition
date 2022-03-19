d6b0bd186d7cb42609d701cb6e6bab4439e25f9d
Adachi Masakazu <marscode48@gmail.com>
2022-03-19 14:30:41 +0900

    javascriptでハンバーガーメニューを作成

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,39 @@
-/*=================================================
-jQuery
-===================================================*/
+document.addEventListener('DOMContentLoaded', function () {
+  const main = new Main();
+});
 
+class Main {
+  constructor() {
+    this._init();
+  }
 
-/*=================================================
-JavaScript
-===================================================*/
+  _init() {
+    new MenuOpen();
+  }
+}
+
+class MenuOpen {
+  constructor() {
+    this.DOM = {};
+    // this.DOM.header = document.querySelector('.header');
+    this.DOM.hamburger = document.querySelector('.header__hamburger');
+    this.DOM.navi = document.querySelector('.header__navi');
+    console.log(this.DOM.navi)
+    this.eventType = this._getEventType();
+    this._addEvent();
+  }
+
+  _getEventType() {
+    return window.ontouchstart ? "touchstart" : "click";
+  }
+
+  _toggle() {
+    this.DOM.hamburger.classList.toggle('active');
+    this.DOM.navi.classList.toggle('active');
+  }
+
+  _addEvent() {
+    this.DOM.hamburger.addEventListener(this.eventType, this._toggle.bind(this));
+    this.DOM.navi.addEventListener(this.eventType, this._toggle.bind(this));    
+  }
+}
