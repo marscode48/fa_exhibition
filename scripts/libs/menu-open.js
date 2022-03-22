2b30e055c574f1294d7a5f79e06128ea0c960032
Adachi Masakazu <marscode48@gmail.com>
2022-03-22 15:14:06 +0900

    javascriptでmenu-openを作成

---
+++ scripts/libs/menu-open.js
@@ -0,0 +1,25 @@
+class MenuOpen {
+  constructor() {
+    this.DOM = {};
+    // this.DOM.header = document.querySelector('.header');
+    this.DOM.hamburger = document.querySelector('.header__hamburger');
+    this.DOM.navi = document.querySelector('.header__navi');
+    console.log(this.DOM.navi)
+    this.eventType = this._getEventType();
+    this._addEvent();
+  }
+
+  _getEventType() {
+    return window.ontouchstart ? "touchstart" : "click";
+  }
+
+  _toggle() {
+    this.DOM.hamburger.classList.toggle('active');
+    this.DOM.navi.classList.toggle('active');
+  }
+
+  _addEvent() {
+    this.DOM.hamburger.addEventListener(this.eventType, this._toggle.bind(this));
+    this.DOM.navi.addEventListener(this.eventType, this._toggle.bind(this));    
+  }
+}
