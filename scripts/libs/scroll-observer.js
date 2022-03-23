226ecf8972e06bb89a735babdce1314f1d2a9bb4
Adachi Masakazu <marscode48@gmail.com>
2022-03-23 17:55:56 +0900

    javascriptでフェード表示を作成

---
+++ scripts/libs/scroll-observer.js
@@ -0,0 +1,42 @@
+class ScrollObserver {
+  constructor(els, cb, options) {
+    console.log(cb)
+    this.els = document.querySelectorAll(els);
+    console.log(this.els)
+    const defaultOptions = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0,
+      once: true
+    };
+    this.cb = cb;
+    this.options = Object.assign(defaultOptions, options);
+    this.once = this.options.once;
+    this._init();
+  }
+  _init() {
+    const callback = function(entries, observer) {
+      console.log(entries)
+      console.log(observer)
+      entries.forEach(entry => {
+        if(entry.isIntersecting) {
+          this.cb(entry.target, true);
+          if(this.once) {
+            console.log(this.once)
+            observer.unobserve(entry.target);
+          }
+        } else {
+          this.cb(entry.target, false);
+        }
+      });
+    };
+
+    this.io = new IntersectionObserver(callback.bind(this), this.options);
+
+    // @see https://github.com/w3c/IntersectionObserver/tree/master/polyfill
+    console.log(this.io.POLL_INTERVAL)
+    this.io.POLL_INTERVAL = 100;
+
+    this.els.forEach(el => this.io.observe(el));
+  }
+}
--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,25 @@ document.addEventListener('DOMContentLoaded', function () {
 class Main {
   constructor() {
     this._init();
+    this._scrollInit();
   }
 
   _init() {
     new MenuOpen();
     new SmoothScroll(0);
   }
+
+  _inviewAnimation(el, inview) {
+    console.log(el)
+    console.log(inview)
+    if(inview) {
+      el.classList.add('show');
+    }
+  }
+
+  _scrollInit() {
+    new ScrollObserver('.inview', this._inviewAnimation);
+  }
 }
+
 
